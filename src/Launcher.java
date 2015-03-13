9ae683d24977fa223183c1c3197659688a22b186
jiu9x9uij <jiu9x9uij@gmail.com>
2015-03-13 16:46:09 -0400

    test topic api

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import org.json.simple.JSONArray;
@@ -15,7 +16,7 @@ import com.jayway.jsonpath.JsonPath;
 
 
 public class Launcher {
-
+	
 	public static Properties properties = new Properties();
 	  public static void main(String[] args) {
 	    try {
@@ -23,19 +24,21 @@ public class Launcher {
 	      HttpTransport httpTransport = new NetHttpTransport();
 	      HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
 	      JSONParser parser = new JSONParser();
-	      GenericUrl url = new GenericUrl("https://www.googleapis.com/freebase/v1/search");
-	      url.put("query", "Cee Lo Green");
-	      url.put("filter", "(all type:/music/artist created:\"The Lady Killer\")");
-	      url.put("limit", "10");
-	      url.put("indent", "true");
+	      String topicId = "/m/0d6lp";
+	      GenericUrl url = new GenericUrl("https://www.googleapis.com/freebase/v1/topic" + topicId);
+//	      url.put("query", "bill gates");
+	      url.put("filter", "suggest");
+//	      url.put("limit", "10");
+//	      url.put("indent", "true");
 	      url.put("key", properties.get("API_KEY"));
 	      HttpRequest request = requestFactory.buildGetRequest(url);
 	      HttpResponse httpResponse = request.execute();
-	      JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
-	      JSONArray results = (JSONArray)response.get("result");
-	      for (Object result : results) {
-	        System.out.println(JsonPath.read(result,"$.name").toString());
-	      }
+	      JSONObject topic = (JSONObject)parser.parse(httpResponse.parseAsString());
+	      System.out.println(JsonPath.read(topic,"$.property['/type/object/type']").toString());
+//	      JSONArray results = (JSONArray)response.get("result");
+//	      for (Object result : results) {
+//	        System.out.println(JsonPath.read(result,"$.name").toString());
+//	      }
 	    } catch (Exception ex) {
 	      ex.printStackTrace();
 	    }
